0b1ebb648017f8cb443e8e187c0ffea7e8b61737
n1cole3 <156975609+n1cole3@users.noreply.github.com>
2024-02-07 20:19:32 -0600

    ejercicios
    semana 2

---
+++ src/main/java/tienda/demo/TiendaNicoleChavesApplication.java
@@ -0,0 +1,13 @@
+package tienda.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaNicoleChavesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaNicoleChavesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tienda/demo/TiendaNicoleChavesApplicationTests.java
@@ -0,0 +1,13 @@
+package tienda.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaNicoleChavesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
